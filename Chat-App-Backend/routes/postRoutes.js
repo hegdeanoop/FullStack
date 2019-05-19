5b6dab7dc3f604427f2b99e36237985c88074033
anoopboogie575@gmail.com <anoopboogie575@gmail.com>
2019-05-19 10:50:14 +0530

    new files

---
+++ Chat-App-Backend/routes/postRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+const PostCtrl = require('../controllers/posts');
+
+router.post('/post/add-post', PostCtrl.AddPost);
+module.exports = router;
