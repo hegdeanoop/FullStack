57155e3b5ad60af7d92aacca53b649112e67dc72
anoopboogie575@gmail.com <anoopboogie575@gmail.com>
2019-05-19 10:44:49 +0530

    del

--- Chat-App-Backend/models/userModels.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = mongoose.Schema({
-    username: { type: String },
-    email: { type: String },
-    password: { type: String }
-});
-
-module.exports = mongoose.model('User', userSchema);
--- Chat-App-Backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const AuthCtrl = require('../controllers/auth');
-
-router.post('/register', AuthCtrl.CreateUser);
-router.post('/login', AuthCtrl.LoginUser);
-
-module.exports = router;
