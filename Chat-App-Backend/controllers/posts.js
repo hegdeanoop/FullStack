f53ef49b57358e929b29c2c7205baf69fc6ac75a
anoopboogie575@gmail.com <anoopboogie575@gmail.com>
2019-06-13 07:56:30 +0530

    all

--- Chat-App-Backend/controllers/posts.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    AddPost(req, res) {
-    console.log(req.body);
-  }
-};
--- Chat-App-Backend/routes/postRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const PostCtrl = require('../controllers/posts');
-
-router.post('/post/add-post', PostCtrl.AddPost);
-module.exports = router;
